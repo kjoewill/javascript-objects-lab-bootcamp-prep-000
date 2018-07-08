b3c83d4fc72d51558ee1dde32d0e274ae895454e
kjoewill@gmail.com <kjoewill@gmail.com>
2018-07-08 18:03:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function deleteFromObjectByKey(object, key) {
   return newObj
 }
 
-function destructiveDeleteFromObjectByKey(object, key) {
+function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key]
   return object
 }
