29df5058281913c1743a036bd3f4ca3394a20814
kjoewill@gmail.com <kjoewill@gmail.com>
2018-07-08 00:29:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign(object, {[key]: value})
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object)
+  delete object[key]
+}
